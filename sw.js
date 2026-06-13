12bcec8c2b7846f84bdc8f5f34311240da8f6c37
huahai-huahai <huahai@example.com>
2026-06-14 02:10:48 +0800

    语音播报价格计算器 - 买菜算账，输入斤数和单价自动报总价

---
+++ sw.js
@@ -0,0 +1,34 @@
+// 买菜算账 - Service Worker (离线缓存)
+var CACHE = 'voice-calc-v1';
+
+self.addEventListener('install', function (e) {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(function (cache) {
+      return cache.addAll([
+        './index.html',
+        './manifest.json',
+        './icon.svg'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(
+        keys.filter(function (k) { return k !== CACHE; })
+            .map(function (k) { return caches.delete(k); })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    fetch(e.request).catch(function () {
+      return caches.match(e.request);
+    })
+  );
+});
